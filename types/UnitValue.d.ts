fb8c4909ccb5dc1e41fc7940861811358a736ed6
fsjohnhuang <fsjohnhuang@hotmail.com>
2021-01-29 09:15:25 +0800

    feat: Extends UnitValue from Number

--- types/UnitValue.d.ts
+++ types/UnitValue.d.ts
@@ -1,6 +1,13 @@
 type Unit = 'px' | 'pt' | 'in' | 'pc' | 'cm' | '%' | 'mm'
 
-declare interface UnitValue {
+/**
+ * Notes:
+ * 1. Addition and substraction on different units would return `NaN`.
+ * 2. Multipilication and division on different units, all elements would convert to the unit as the same as the first factor.
+ * 3. Relational Operators on different units work as that on the same untis.
+ * 4. TypeScript's rule: The left-hand side and the right-hand side of an arithmetic operator must be of type `any`, `number`, `bigint` or an `enum` type.
+ */
+declare interface UnitValue extends Number {
     /**
      * Converts to the value represented in indicated unit without change the original one.
      * @example
