b9055ebd1b4c59c9b65970c56bc32865694a933c
fsjohnhuang <fsjohnhuang@hotmail.com>
2021-05-04 16:06:38 +0800

    update

--- types/UnitValue.d.ts
+++ types/UnitValue.d.ts
@@ -5,7 +5,17 @@ type Unit = 'px' | 'pt' | 'in' | 'pc' | 'cm' | '%' | 'mm'
  * 1. Addition and substraction on different units would return `NaN`.
  * 2. Multipilication and division on different units, all elements would convert to the unit as the same as the first factor.
  * 3. Relational Operators on different units work as that on the same untis.
- * 4. TypeScript's rule: The left-hand side and the right-hand side of an arithmetic operator must be of type `any`, `number`, `bigint` or an `enum` type.
+ * TypeScript's rules:
+ * 1. Operator '>' cannot be applied to types 'UnitValue' and 'number'.
+ * 2. The left-hand side and the right-hand side of an arithmetic operator must be of type `any`, `number`, `bigint` or an `enum` type.
+ * 3. Converion of type `number` to type `UnitValue` may not be a mistake because neither type sufficiently overlaps with the other.
+ * Solutions for TypeScript's rules:
+ * ```
+ * let a = UnitValue(1,'px'), b = UnitValue(2, 'px')
+ * let c1: UnitValue = <UnitValue><Number>(<any>a + <any>b)
+ * let c2: UnitValue = <UnitValue><Number>(<number><Number>a + <number><Number>b)
+ * let c3: UnitValue = <UnitValue><unknow>(<any>a + <any>b)
+ * ```
  */
 declare interface UnitValue extends Number {
     /**
